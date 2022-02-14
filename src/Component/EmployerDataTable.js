fd83df2307283972d6c83813fc67c694d8864b90
Maruf Islam <maruf15-1591@diu.edu.bd>
2022-02-15 03:03:57 +0600

    f-commit

---
+++ src/Component/EmployerDataTable.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const EmployerDataTable = (props) => {
+  const employerData = props.employers.employers;
+  //   console.log(employerData);
+  return (
+    <div className="md:w-4/6 w-11/12 m-auto">
+      <p className="text-center my-3 font-bold">Employers Data Table</p>
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border border-gray-300 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Position
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Office Location
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Age
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Date
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {employerData.map((person) => (
+                    <tr key={person.Name}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="ml-4">
+                            <div className="text-sm font-medium text-gray-900">
+                              {person.Name}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {person.Position}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {person.OfficeLocation}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.Age}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.Date}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployerDataTable;
---
+++ src/Component/Home.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import EmployerDataTable from "./EmployerDataTable";
+
+const Home = () => {
+  const [readExcelFile, setReadExcelFile] = useState({});
+  const [excelData, setExcelData] = useState({});
+  const [error, setError] = useState(null);
+  const [getExcelDataFromDb, setGetExcelDataFromDb] = useState(null);
+  const fileType = ["application/vnd.ms-excel"];
+
+  const handleFile = (e) => {
+    let file = e.target.files[0];
+    if (file && fileType.includes(file.type)) {
+      let fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = (e) => {
+        setReadExcelFile(e.target.result);
+        setError(null);
+      };
+    } else {
+      setError("Only xls files allwed!");
+    }
+    // console.log(file);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (readExcelFile) {
+      const workbook = XLSX.read(readExcelFile, { type: "buffer" });
+      const worksheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[worksheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+      const dataField = {
+        title: "Employee Field",
+        employers: data,
+      };
+
+      //post employer data
+
+      axios.post("http://localhost:5000/employer/addEmployee", dataField).then(
+        (res) => {
+          console.log(res);
+          setReadExcelFile(null);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+
+      //get posted data
+
+      axios.get("http://localhost:5000/employer/getEmployee").then(
+        (res) => {
+          if (res.data) {
+            let getData = res.data;
+            let dataLength = getData.length;
+            setGetExcelDataFromDb(getData[dataLength - 1]);
+            setReadExcelFile(null);
+          } else {
+            setGetExcelDataFromDb(null);
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      setExcelData(null);
+    }
+  };
+  return (
+    <div
+      className="w-full bg-blue-50 h-screen"
+    >
+      <div className="md:w-2/6 w-11/12 m-auto">
+        <div>
+          <p className="text-center p-5 font-bold">Handle Excel Data</p>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              type="file"
+              onChange={handleFile}
+            />
+            <button
+              type="submit"
+              className="text-center w-full bg-blue-500 hover:bg-blue-600 text-white px-5 py-2 my-5"
+            >
+              SUBMIT
+            </button>
+          </form>
+        </div>
+
+        {/* Display error message */}
+
+        {error && (
+          <div className="flex w-full mx-auto overflow-hidden bg-red-100">
+            <div className="flex items-center justify-center w-14 bg-red-500">
+              <FontAwesomeIcon
+                className="text-white"
+                icon={faCircleExclamation}
+              />
+            </div>
+            <div className="px-2 py-2 -mx-3">
+              <div className="mx-3">
+                <span className="text-red-500 font-semibold">{error}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      {getExcelDataFromDb ? (
+        <EmployerDataTable employers={getExcelDataFromDb}></EmployerDataTable>
+      ) : (
+        <p className="text-center text-sm text-red-500">
+          Please select a xecel(xls) file and view on the table.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Home;
